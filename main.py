27d9e5a0a4fda0799ef8c143ee025191578dabde
henry <henry2.900109@gmail.com>
2024-01-03 15:41:55 +0800

    money

--- main.py
+++ main.py
@@ -1,28 +1,136 @@
 import flet as ft
+import pay
+from time import sleep
 
-def main(page : ft.Page):
-    # page.add(ft.SafeArea(ft.Text("hello world")))
+
+
+
+
+def main(page: ft.Page):
     page.title = "Flet Count App"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.NONE
+    # page.debug = True
+    page.padding = 20
 
-    number_textbox = ft.TextField(value="0",text_align = ft.TextAlign.CENTER,width=100)
+    data = {}
+    
+    def button1_clicked(e):
+        name = []
+        t = ft.Text() 
+        if tb.value == "":
+            t.value = "請輸入人名"
+        elif len(tb.value.split("\n")) != int(people.value):
+            t.value = "清輸入正確數量"
+        else:
+            for item in tb.value.split("\n"):
+                name.append(item)
+            personlist = pay.Person_init(int(people.value),name)
+            t.value =str(personlist)
+            for i in range (int(people.value)):
+                data[f"{name[i]}"] = {}
+            page.remove(people)
+            page.remove(tb)
+            page.remove(bn1)
+            # page.remove(t)
+            # columns=[ft.DataColumn(ft.Text("name"))],
+            # for i in range(int(people.value)):
+                # table = ft.DataTable(rows=[ft.DataRow(cells=[ft.DataCell(ft.Text(name[i]))])])
+                # page.add(table)
+            # radio_buttons = [ft.Radio(value=f"{name[i]}", label=f"{name[i]}") for i in range(int(people.value))]
+            # radio_group = ft.RadioGroup(content=ft.Column(radio_buttons))
+            # page.add(radio_group)
+            # t = ft.Text()
+            # t.value = "花了"
+            # d = ft.Text()
+            # d.value = "元"
+            # page.add(ft.Row([t,number_textbox,d])) 
 
-    def minus_click(e):
-        if int(number_textbox.value) > 0:
-            number_textbox.value = str(int(number_textbox.value)-1)
-            page.update()
+            def button2_clicked(e):
+                data[d.value][number_textbox.value] = option_textbox.value
+                print(data)
+                option_textbox.value = ""
+                number_textbox.value = ""
+                page.update()
 
-    def plus_click(e):
-            number_textbox.value = str(int(number_textbox.value)+1)
-            page.update()
+            # def find_option(option_name):
+            #     for option in d.options:
+            #         if option_name == option.key:
+            #             return option
+            #     return None
 
-    page.add(ft.Row([
-        ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-        number_textbox,
-        ft.IconButton(ft.icons.ADD, on_click=plus_click)
-    ],alignment=ft.MainAxisAlignment.CENTER))
+            # def add_clicked(e):
+            #         d.options.append(ft.dropdown.Option(option_textbox.value))
+            #         d.value = option_textbox.value
+            #         option_textbox.value = ""
+            #         page.update()
+
+            # def delete_clicked(e):
+            #     option = find_option(d.value)
+            #     if option != None:
+            #         d.options.remove(option)
+            #         # d.value = None
+            #         page.update()
 
 
-# ft.app(target=main, view=ft.)
-# ft.app(target=main, view=ft.WEB_BROWSER ,port=5001)
+
+
+
+            def button3_clicked(e):
+                pm = pay.PayList(data,int(people.value))
+                result = pay.result(personlist,pm)
+                page.remove(option_textbox)
+                page.remove(d)
+                page.remove(number_textbox)
+                t = ft.Text()
+                t.value = result
+                page.add(t)
+                page.remove(row)
+
+
+                page.update()
+
+            d = [ft.dropdown.Option(f"{name[i]}") for i in range(int(people.value))]
+            d = ft.Dropdown(label="人名", options = d)
+            number_textbox = ft.TextField(hint_text="品項", text_align=ft.TextAlign.CENTER, width=100)
+
+            option_textbox = ft.TextField(hint_text="Enter item name")
+            bn2 = ft.ElevatedButton(text="繼續", on_click=button2_clicked)
+            bn3 = ft.ElevatedButton(text="結束輸入", on_click=button3_clicked)
+            page.add(d,number_textbox,option_textbox)
+            row = ft.Row([bn2,bn3])
+            page.add(row) 
+        page.update()
+
+
+
+ 
+
+    people = ft.Dropdown(label="人數", options=[
+        ft.dropdown.Option("1"),
+        ft.dropdown.Option("2"),
+        ft.dropdown.Option("3"),
+        ft.dropdown.Option("4"),
+    ])
+
+    tb = ft.TextField(
+            label="請輸入人名",
+            hint_text="Name1\nName2",
+            multiline=True,
+            filled=True,
+            min_lines=0,
+            max_lines=10,
+        )
+
+
+
+    bn1 = ft.ElevatedButton(text="Submit", on_click=button1_clicked)
+    t = ft.Text()
+
+    # page.add(ft.Row([
+    #     people,
+    # ]))
+    # , alignment=ft.MainAxisAlignment.CENTER)
+    page.add(people,tb,bn1,t)
+
+
 ft.app(target=main, view=None)
---
+++ pay.py
@@ -0,0 +1,54 @@
+def Person_init(num:int, person:list):
+    expenses = {}
+
+    num_people = num
+
+    for i in range(num):
+        # person = input(f"請輸入第{i+1}個人的名字：")
+        expenses[person[i]] = None
+
+    for person in expenses.keys():
+        expenses[person] = {item: 0 for item in expenses.keys() if item != person}
+
+    return expenses
+
+
+ 
+def PayList(paylist,num):
+
+    paylist = {name: sum(int(money) for person, money in items.items()) / num for name, items in paylist.items()}
+
+    # paylist = {name: sum(int(money) / num) for name, items in paylist.items() for person, money in items.items()}
+    print(paylist)
+
+    return paylist
+
+
+
+# person = Person_init(num,person)  
+# pay = PayList(len(person.keys()))
+def result(person,pay):
+    for item in pay.keys():
+        # print(f"每個人給{item} {pay[item]}元")
+        for person_key in person.keys():
+            # print(f"--{person_key}--")
+
+            if item != person_key:
+                person[person_key][item] = person[person_key][item] + pay[item]
+                # print(person)
+            else:
+                person[item] = {person: items - pay[item] for person, items in person[person_key].items()}
+                # print(person)
+
+    result = ""
+    for person_key in person:
+        result += f"{person_key}\n"
+        for p,item in person[person_key].items():
+            if item > 0:
+                result += f"需給 {p} : {item}元\n"
+            elif item < 0:
+                result += f"跟 {p} 拿 : {item}元\n"
+
+        result += "\n\n"
+    return result
+    print(result)
